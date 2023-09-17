df0457ad0e0605e727ae07938f2b31d7d59c1156
EWP80 <erikpw009@gmail.com>
2023-09-16 21:43:58 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Unix_shell project
+
+This project implements a simple shell command pipeline executor, which allows the execution of multiple commands chained by pipes and handles input/output redirections. The primary components include functions for parsing the command-line inputs, executing single commands, and managing pipelines of commands.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## Prerequisites
+
+- GCC Compiler (or any compatible C compiler)
+- Linux/Unix Environment (as it uses fork, pipe and exec which are specific to UNIX-like systems)
+
+## Installation
+
+- Clone the repository or download the source files.
+- Navigate to the project directory.
+- Compile the source files using a C compiler:
+
+```
+gcc -o pipeline_executor main.c
+```
+
+- Run the executable:
+
+```
+./pipeline_executor
+```
+
+## Usage
+
+Once you run the executable, you can enter commands in the following format:
+
+```
+command1 [arg1 arg2 ...] [| command2 [arg1 arg2 ...]] [< input_file] [> output_file] [>> append_output_file] [&]
+```
+
+| - Pipe symbol to chain multiple commands. The output of the preceding command will be the input to the following command.
+< - Redirect input from a file.
+
+> - Redirect output to a file (overwrite if file exists).
+>   > - Append output to a file.
+>   >   & - Run command in the background (not wait for it to finish).
+
+- For example:
+
+```
+cat file.txt | grep "keyword" > output.txt
+```
+
+## Code Structure
+
+The project consists of three primary functions:
+
+- parse - Parses a command string to extract individual commands and their arguments, as well as input/output redirection specifications.
+- execute - Forks a new process to execute the parsed command or chain of commands.
+- execute_pipeline - Manages the execution of multiple commands connected by pipes.
+
+## Functions Documentation
+
+int parse(char* s, char* argv[MAX_PIPE_COMMANDS][MAX_CMD_LINE_ARGS])
+
+- s: A string containing the entire command input.
+- argv: A 2D array to store the commands and their arguments.
+- Return: The number of commands parsed.
+- int execute(char\* input)
+
+- input: A string containing the entire command input.
+- Return: 0 on success, -1 on failure.
+- void execute_pipeline(char\* args[MAX_PIPE_COMMANDS][MAX_CMD_LINE_ARGS], int n)
+
+- args: A 2D array containing the commands and their arguments.
+- n: The number of commands to execute.
+
+## Contributor
+
+Erik Williams
---
+++ shell.c
@@ -0,0 +1,283 @@
+#include <sys/wait.h>
+
+#include <stdio.h>
+
+#include <string.h>
+
+#include <stdlib.h>
+
+#include <stdbool.h>
+
+#include <sys/types.h>
+
+#include <unistd.h>
+
+#include <fcntl.h>
+
+#define MAX_CMD_LINE_ARGS 128
+#define MAX_PIPE_COMMANDS 10
+
+// Function to return the minimum of two integers
+int min(int a, int b) {
+  return a < b ? a : b;
+}
+
+// Function to redirect the standard input to a file
+void redirect_input(const char * filename) {
+  int fd = open(filename, O_RDONLY);
+  if (fd == -1) {
+    perror("open filename in redirect_input failed");
+    exit(EXIT_FAILURE);
+  }
+  dup2(fd, STDIN_FILENO);
+  close(fd);
+}
+
+// Function to redirect the standard output to a file, with an optional append mode
+void redirect_output(const char * filename, bool append) {
+  int flags = O_WRONLY | O_CREAT;
+  flags |= append ? O_APPEND : O_TRUNC;
+  int fd = open(filename, flags, 0644);
+  if (fd == -1) {
+    perror("open failure in redirect_output");
+    exit(EXIT_FAILURE);
+  }
+  dup2(fd, STDOUT_FILENO);
+  close(fd);
+}
+
+// Function to execute a pipeline of two commands
+void execute_pipeline(char * args[MAX_PIPE_COMMANDS][MAX_CMD_LINE_ARGS], int n) {
+  int i, pipefds[2 * n];
+
+  // Create n pipes
+  for (i = 0; i < n; i++) {
+    if (pipe(pipefds + i * 2) < 0) {
+      perror("Couldn't pipe");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  int pid;
+  int status;
+
+  for (i = 0; i < n + 1; i++) {
+    pid = fork();
+    if (pid == 0) {
+      // Not the first command
+      if (i != 0) {
+        if (dup2(pipefds[(i - 1) * 2], 0) < 0) {
+          exit(EXIT_FAILURE);
+        }
+      }
+
+      // Not the last command
+      if (i != n) {
+        if (dup2(pipefds[i * 2 + 1], 1) < 0) {
+          exit(EXIT_FAILURE);
+        }
+      }
+
+      // Close all pipe file descriptors
+      for (int j = 0; j < 2 * n; j++) {
+        close(pipefds[j]);
+      }
+
+      // Execute the command
+      if (execvp(args[i][0], args[i]) < 0) {
+        perror("exec failed");
+        exit(EXIT_FAILURE);
+      }
+    } else if (pid < 0) {
+      perror("error forking");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Close all pipe file descriptors in parent
+  for (i = 0; i < 2 * n; i++) {
+    close(pipefds[i]);
+  }
+
+  // Wait for all child processes to finish
+  for (i = 0; i < n + 1; i++) {
+    wait( & status);
+  }
+}
+
+// Function to parse the command line input and handle redirections and pipe
+int parse(char * s, char * argv[MAX_PIPE_COMMANDS][MAX_CMD_LINE_ARGS]) {
+  char * token = strtok(s, " \t\n\v\f\r");
+  char * input_file = NULL;
+  char * output_file = NULL;
+  bool append = false;
+  int cmd_count = 0;
+  int arg_count = 0;
+
+  while (token != NULL) {
+    if (strcmp(token, "<") == 0) {
+      token = strtok(NULL, " \t\n\v\f\r");
+      input_file = token;
+    } else if (strcmp(token, ">") == 0) {
+      token = strtok(NULL, " \t\n\v\f\r");
+      output_file = token;
+    } else if (strcmp(token, ">>") == 0) {
+      token = strtok(NULL, " \t\n\v\f\r");
+      output_file = token;
+      append = true;
+    } else if (strcmp(token, "|") == 0) {
+      cmd_count++;
+      arg_count = 0;
+    } else {
+      argv[cmd_count][arg_count] = token;
+      arg_count++;
+    }
+    token = strtok(NULL, " \t\n\v\f\r");
+  }
+
+  argv[cmd_count][arg_count] = NULL;
+
+  if (input_file != NULL) {
+    redirect_input(input_file);
+  }
+
+  if (output_file != NULL) {
+    redirect_output(output_file, append);
+  }
+
+  return cmd_count;
+}
+
+// Function to execute a parsed command
+int execute(char * input) {
+  char * shell_argv[MAX_PIPE_COMMANDS][MAX_CMD_LINE_ARGS];
+
+  // Initialize shell_argv to NULL
+  for (int i = 0; i < MAX_PIPE_COMMANDS; i++) {
+    for (int j = 0; j < MAX_CMD_LINE_ARGS; j++) {
+      shell_argv[i][j] = NULL;
+    }
+  }
+
+  int shell_argc = parse(input, shell_argv);
+
+  int status = 0;
+  bool run_in_background = false;
+
+  if (shell_argc > 0 && strcmp(shell_argv[shell_argc - 1][0], "&") == 0) {
+    shell_argv[shell_argc - 1][0] = NULL; // Remove "&" from arguments
+    run_in_background = true;
+  }
+
+  if (shell_argc > 0) {
+    execute_pipeline(shell_argv, shell_argc);
+  } else {
+    pid_t pid = fork();
+    if (pid < 0) {
+      fprintf(stderr, "Fork() failed\n");
+      return -1;
+    } else if (pid == 0) {
+      // Child process
+      if (execvp(shell_argv[0][0], (char *
+          const * ) shell_argv[0]) < 0) {
+        perror("execvp failed");
+        exit(EXIT_FAILURE);
+      }
+    } else {
+      // Parent process
+      if (!run_in_background) {
+        while (wait( & status) != pid) {}
+      }
+    }
+  }
+  return 0;
+}
+
+int main(int argc,
+  const char * argv[]) {
+  char input[BUFSIZ];
+  char last_input[BUFSIZ];
+  bool finished = false;
+  memset(last_input, 0, BUFSIZ * sizeof(char));
+
+  // Main loop to read and execute commands
+  while (!finished) {
+    memset(input, 0, BUFSIZ * sizeof(char));
+    printf("osh > ");
+    fflush(stdout);
+
+    if (fgets(input, BUFSIZ, stdin) == NULL) {
+      if (feof(stdin)) {
+        // End of file reached, exit gracefully
+        break;
+      }
+      fprintf(stderr, "no command entered\n");
+      continue; // Skip the rest of the loop iteration
+    }
+
+    size_t len = strlen(input);
+    if (input[strlen(input) - 1] == '\n') {
+      input[strlen(input) - 1] = '\0';
+    }
+
+    // Handling exit and history (!!) commands
+    if (strncmp(input, "exit", 4) == 0) {
+      finished = true;
+    } else if (strncmp(input, "!!", 2) == 0) {
+      if (strlen(last_input) > 0) {
+        execute(last_input);
+      } else {
+        printf("No commands in history.\n");
+      }
+    } else {
+      // Store the last command and execute the current command
+      strncpy(last_input, input, min(strlen(input), BUFSIZ - 1));
+      execute(input);
+    }
+  }
+
+  printf("\t\t...exiting\n");
+  return 0;
+}
+
+/*
+Output showing the program running:
+osh > ls -l
+(total files and directories listed)
+osh > cat < input.txt > output.txt
+osh > ls -l | grep "output.txt"
+(output.txt file details shown)
+osh > !!
+(output.txt file details shown again)
+osh > exit
+        ...exiting
+
+
+
+
+epw@EPWPC:~/shell_project$ gcc -o shell shell.c
+epw@EPWPC:~/shell_project$ sudo ./shell
+osh > ls -l
+total 40
+-rw-r--r-- 1 epw  epw    183 Sep 16 12:49  input.txt
+-rw-r--r-- 1 root root    12 Sep 16 12:55 'out.txt),'
+-rw-r--r-- 1 root root   189 Sep 16 12:55  output.txt
+-rwxr-xr-x 1 epw  epw  17240 Sep 16 13:00  shell
+-rw-r--r-- 1 epw  epw   5671 Sep 16 13:00  shell.c
+osh > cat < input.txt > output.txt
+epw@EPWPC:~/shell_project$ sudo ./shell
+osh > ls -l | grep "output.txt"
+total 36
+-rw-r--r-- 1 epw  epw    183 Sep 16 12:49 input.txt
+-rw-r--r-- 1 root root   202 Sep 16 13:01 output.txt
+-rwxr-xr-x 1 epw  epw  17240 Sep 16 13:00 shell
+-rw-r--r-- 1 epw  epw   5671 Sep 16 13:00 shell.c
+osh > !!
+total 36
+-rw-r--r-- 1 epw  epw    183 Sep 16 12:49 input.txt
+-rw-r--r-- 1 root root   202 Sep 16 13:01 output.txt
+-rwxr-xr-x 1 epw  epw  17240 Sep 16 13:00 shell
+-rw-r--r-- 1 epw  epw   5671 Sep 16 13:00 shell.c
+osh > exit
+                ...exiting
+*/
